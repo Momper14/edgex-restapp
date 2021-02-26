3a959c6716fe255180333e642d4415160c6cbad2
Moritz Momper <moritz.momper@gmail.com>
2021-02-26 11:25:21 +0100

    moved init to main package

--- cmd/edgex-restapp-server/init.go
+++ cmd/edgex-restapp-server/init.go
@@ -1,8 +1,7 @@
-package restapi
+package main
 
 // this file helps to have the init package as first import
 
 import (
-	// init without changing main
 	_ "github.com/momper14/edgex-restapp/init"
 )
